b80f7c1c463074dc69a74c589e21808ff578b575
m4xine <maxined@pm.me>
2023-05-26 14:47:18 -0400

    Add preprocessor definition to not define `main`

--- plush/main.cpp
+++ plush/main.cpp
@@ -1,4 +1,6 @@
 // SPDX-FileCopyrightText: Copyright (c) 2023, Maxine DeAndrade
 // SPDX-License-Identifier: BSD-3-Clause
 
+#ifndef PLUSH_NOMAIN
 int main(int argc, char **argv) { return 0; }
+#endif // PLUSH_NOMAIN
